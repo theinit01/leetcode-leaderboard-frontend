6fe206faf28caee73361c17a8c941d979b799bb1
theinit01 <tushar3145@gmail.com>
2024-07-10 16:55:01 +0530

    Added Spinner

--- src/components/Leaderboard.jsx
+++ src/components/Leaderboard.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Container, Card, Button, Row, Col } from 'react-bootstrap';
+import { Container, Card, Button, Row, Col, Spinner } from 'react-bootstrap';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './Leaderboard.css'; // Import your custom CSS file
@@ -8,12 +8,14 @@ import './Leaderboard.css'; // Import your custom CSS file
 const Leaderboard = () => {
   const [users, setUsers] = useState([]);
   const [sortType, setSortType] = useState('total');
+  const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
     fetchLeaderboard(sortType);
   }, [sortType]);
 
   const fetchLeaderboard = (type) => {
+    setLoading(true);
     axios.get(`https://leetcode-one.vercel.app/leaderboard/${type}`)
       .then(response => {
         // Assign ranks based on the index of each user
@@ -22,10 +24,12 @@ const Leaderboard = () => {
           rank: index + 1
         }));
         setUsers(rankedUsers);
+        setLoading(false);
       })
       .catch(error => {
         console.error(`Error fetching ${type} leaderboard:`, error);
         toast.error(`Error fetching ${type} leaderboard: ${error.message}`);
+        setLoading(false);
       });
   };
 
@@ -66,27 +70,33 @@ const Leaderboard = () => {
         <Button variant="secondary" className="me-2" onClick={() => setSortType('hard')}>Hard</Button>
         <Button variant="secondary" onClick={() => setSortType('total')}>Total</Button>
       </div>
-      <Row className="justify-content-center">
-        {users.map((user, index) => (
-          <Col key={user.username} xs={10} md={10} lg={7} className="mb-2">
-            <Card className="leaderboard-card" id={`${getCardVariant(index + 1)}`}>
-              <Card.Body className="d-flex justify-content-between align-items-center">
-                <div className="rank">
-                  <Card.Text className="mb-0">#{user.rank} &nbsp;</Card.Text>
-                </div>
-                <div className="username">
-                  <Card.Title className="mb-0">{user.username}</Card.Title>
-                </div>
-                <div className="questions">
-                  <Card.Text className="mb-0">
-                    {renderQuestions(user)}
-                  </Card.Text>
-                </div>
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
-      </Row>
+      {isLoading ? ( // Conditional rendering based on isLoading state
+        <Spinner animation="border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
+      ) : (
+        <Row className="justify-content-center">
+          {users.map((user, index) => (
+            <Col key={user.username} xs={10} md={10} lg={7} className="mb-2">
+              <Card className="leaderboard-card" id={`${getCardVariant(index + 1)}`}>
+                <Card.Body className="d-flex justify-content-between align-items-center">
+                  <div className="rank">
+                    <Card.Text className="mb-0">#{user.rank} &nbsp;</Card.Text>
+                  </div>
+                  <div className="username">
+                    <Card.Title className="mb-0">{user.username}</Card.Title>
+                  </div>
+                  <div className="questions">
+                    <Card.Text className="mb-0">
+                      {renderQuestions(user)}
+                    </Card.Text>
+                  </div>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      )}
     </Container>
   );
 };
