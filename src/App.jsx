d51d83bf45353d9b2f289080cb0913327440c029
theinit01 <tushar3145@gmail.com>
2024-07-11 18:58:15 +0530

    fixed client side routing probably :)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 import Leaderboard from './components/Leaderboard';
@@ -11,22 +11,20 @@ import './App.css';
 
 function App() {
   return (
-    <HashRouter>
-      <div>
-        <div className="app-container">
-          <NavBar />
-          <div className="main-content">
-            <Routes>
-              <Route path="/" element={<Leaderboard />} />
-              <Route path="/add_user" element={<AddUser />} />
-              <Route path="/users" element={<UsersPage />} />
-              <Route path="/daily_problems" element={<DailyProblems />} />
-            </Routes>
-          </div>
+    <Router>
+      <div className="app-container">
+        <NavBar />
+        <div className="main-content">
+          <Routes>
+            <Route path="/" element={<Leaderboard />} />
+            <Route path="/add_user" element={<AddUser />} />
+            <Route path="/users" element={<UsersPage />} />
+            <Route path="/daily_problems" element={<DailyProblems />} />
+          </Routes>
         </div>
-        <Footer />
       </div>
-    </HashRouter>
+      <Footer />
+    </Router>
   );
 }
 
